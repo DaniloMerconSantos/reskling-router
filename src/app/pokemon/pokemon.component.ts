a20870e7cfdc4e89b2c9ae7bf1152ce6b2aebcbf
Danilo Mercon <dsantos@eaibrasil.com.br>
2022-03-11 23:19:32 -0300

    Favoritos

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -13,6 +13,8 @@ export class PokemonComponent implements OnInit {
   nomePokemon:string = '';
   dataSoucer:RootObject[]= [];
   type: any[] = [];
+  temFavorito: boolean = false;
+  favoritos: RootObject[] = [];
 
   constructor(
     private route: ActivatedRoute,
@@ -24,8 +26,44 @@ export class PokemonComponent implements OnInit {
     this.pokemonService.obterDetalhes(this.nomePokemon)
     .subscribe((poke)=>{
       this.dataSoucer.push(poke);
+      this.verificarFavorito(this.dataSoucer[0]);
     })
-
   }
 
+  addFavorito(data: RootObject) {
+    if(this.verificarFavorito(data)) {
+      this.favoritos = this.favoritos.filter(value=>{
+        return value.id != data.id;
+      })
+      console.log(this.favoritos);
+      if(this.favoritos.length == 0 ){
+        localStorage.removeItem('favoritos');
+        return this.temFavorito = false;
+      }
+      this.favoritos.push(data);
+
+      return localStorage.setItem('favoritos', JSON.stringify(this.favoritos));
+    }
+    this.favoritos.push(data);
+    localStorage.setItem('favoritos', JSON.stringify(this.favoritos));
+    this.temFavorito = true;
+  }
+
+  verificarFavorito(data: RootObject) {
+    var pokemons = localStorage.getItem('favoritos');
+
+    if(pokemons && data){
+      var array: [] = JSON.parse(pokemons);
+
+      const poke = array.filter(value=>{
+        return value['id'] === data.id;
+      })
+      if(poke.length > 0) return this.temFavorito = true;
+    }
+    return  this.temFavorito = false;
+  }
+
+  removeFavorito() {
+    // this.favoritos.splice()
+  }
 }
